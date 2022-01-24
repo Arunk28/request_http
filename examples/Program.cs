761a33d94c07a5d79d4a70aa7ba9f237b6533503
arunk28 <arunkumar280491@gmail.com>
2022-01-24 20:31:49 +0530

    added example how to use this lib ?

---
+++ examples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using request;
+namespace requestclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HttpProperties http = new HttpProperties()
+            {
+                dictObject = null,
+                methodType = "GET",
+                sync = true,
+                token = null,
+                url = "https://jsonplaceholder.typicode.com/todos/1"
+            };
+             object s = Http.HttpRequest(http);
+            Console.WriteLine(s);
+
+        }
+    }
+}
